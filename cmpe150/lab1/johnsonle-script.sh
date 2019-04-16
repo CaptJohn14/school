689a54cdf3c12006a875001b6990f28c225429ee
Johnson <jonle@ucsc.edu>
2019-04-16 11:24:17 -0700

    modified script for more elegance

--- cmpe150/lab1/johnsonle-script.sh
+++ cmpe150/lab1/johnsonle-script.sh
@@ -5,15 +5,9 @@
 # Desc: Iterates all file in directory and echos even numbered lines
 # ==============================================================================
  # !/bin/bash
-FILES=*     #grabs files in directory and throws in list basically
-for file in $FILES      #iterate the list
-do
-    count=1     #reset count to 1. only print when even
-    while read -r line; do
-        if (($count%2==0))      #determines if even number
-        then
-            echo ""$file": "$line""     #print
-        fi
-        let count++
+for file in *; do           #iterates all the files
+    while read -r line; do  #reads a line
+        read -r line        #reads again. Basically ignores the last read.
+        echo ""$file": "$line""
     done < "$file"
 done
