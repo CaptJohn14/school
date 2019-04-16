d7a28ce8d53263638ba7e44fe8004c3bba0e0868
johnson <johnnjacob14@yahoo.com>
2019-04-15 23:26:21 -0700

    yo done

--- cmpe150/lab1/example.py
+++ cmpe150/lab1/example.py
@@ -10,9 +10,15 @@ class MyTopology(Topo):
         Topo.__init__(self)
         # Set Up Topology Here
         switch = self.addSwitch('s1')  # Adds a Switch
-        host1 = self.addHost('h1')  # Adds a Host
-        self.addLink(host1, switch)  # Add a link
 
+        host1 = self.addHost('h1')
+        host2 = self.addHost('h2')
+        host3 = self.addHost('h3')
+        host4 = self.addHost('h4')
+        self.addLink(host1, switch)
+        self.addLink(host2, switch)
+        self.addLink(host3, switch)
+        self.addLink(host4, switch)
 
 if __name__ == '__main__':
     """If this script is run as an executable (by chmod +x), this is
@@ -21,5 +27,6 @@ if __name__ == '__main__':
     topo = MyTopology()  # Creates the topology
     net = Mininet(topo=topo)  # Loads the topology
     net.start()  # Starts Mininet
-    # Commands here will run on the simulated topologyCLI(net)
+    # Commands here will run on the simulated topology
+    CLI(net)
     net.stop()  # Stops Mininet
