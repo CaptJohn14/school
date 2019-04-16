ba75bffaec1009613fb481984adff19eebd6c300
Johnson Le <jonle@ucsc.edu>
2019-04-15 23:29:48 -0700

    dasdasdagfeaFsdGe fgrsf ds f

--- cmpe150/lab1/johnsonle-script.sh
+++ cmpe150/lab1/johnsonle-script.sh
@@ -8,15 +8,12 @@
 FILES=*     #grabs files in directory and throws in list basically
 for file in $FILES      #iterate the list
 do
-    if [ $file != 'johnsonle-script.sh' ]      #skipping the script itself
-    then
-        count=1     #reset count to 1. only print when even
-        while read -r line; do
-            if (($count%2==0))      #determines if even number
-            then
-                echo ""$file": "$line""     #print
-            fi
-            let count++
-        done < "$file"
-    fi
+    count=1     #reset count to 1. only print when even
+    while read -r line; do
+        if (($count%2==0))      #determines if even number
+        then
+            echo ""$file": "$line""     #print
+        fi
+        let count++
+    done < "$file"
 done
